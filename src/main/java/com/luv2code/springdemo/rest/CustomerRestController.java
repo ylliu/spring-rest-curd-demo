bbe311a3c5729e87674fc053553d7af33520bfe4
ylliu <liuming1908@gmail.com>
2023-01-15 15:22:38 +0800

    add customer by POST

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -3,10 +3,7 @@ package com.luv2code.springdemo.rest;
 import com.luv2code.springdemo.entity.Customer;
 import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +26,14 @@ public class CustomerRestController {
         if (theCustomer == null) {
             throw new CustomerNotFoundException("Customer id not found - " + customerId);
         }
+        return theCustomer;
+    }
+
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer theCustomer) {
+        // also just in case they pass an id in JSON ... set id to 0
+        theCustomer.setId(0);
+        customerService.saveCustomer(theCustomer);
         return theCustomer;
     }
 }
